83919e72200d88888ef789b71218e61aa3200d37
Caio Oliveira <caio.oliveira@nubank.com.br>
2017-08-11 15:45:53 -0300

    get quotes from foxbit

--- ledgergrabber/crypto.py
+++ ledgergrabber/crypto.py
@@ -11,6 +11,16 @@ def from_coinmarketcap(base='USD', as_of=None):
 
     quotes = [(x["symbol"], base, x[quote_field], as_of)
               for x in req_results
-              if x[quote_field]]
+              if x[quote_field] and x["symbol"] != base]
 
     return quotes
+
+
+def btc_from_foxbit(base='USD', as_of=None):
+    req_url = "https://api.blinktrade.com/api/v1/" + base + "/ticker"
+
+    as_of = as_of or datetime.now()
+
+    req_results = requests.get(req_url).json()
+
+    return ('BTC', base, req_results['last'], as_of)
