155535c855be19eab97f8c46cdde9bc659e5263d
Caio Oliveira <caio.oliveira@nubank.com.br>
2017-08-13 18:55:28 -0300

    add aliases support

--- ledgergrabber/core.py
+++ ledgergrabber/core.py
@@ -1,15 +1,15 @@
 LINE_FMT = "P %(as_of)s %(symbol)s %(base)s %(quote)s"
 
 
-def to_lines(quotes):
+def to_lines(quotes, aliases={}):
     return [LINE_FMT % {'as_of': as_of.strftime("%Y/%m/%d %H:%M:%S"),
-                        'base': base,
-                        'symbol': symbol,
+                        'base': aliases.get(base, base),
+                        'symbol': aliases.get(symbol, symbol),
                         'quote': quote}
             for symbol, base, quote, as_of in quotes]
 
 
-def to_file(quotes, path):
+def to_file(quotes, path, aliases={}):
     with open(path, 'w') as f:
-        for line in to_lines(quotes):
+        for line in to_lines(quotes, aliases):
             print(line, file=f)
